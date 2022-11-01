7a55e7697865f9a747b4fb7aee6e124cb40ea588
Manish Sajwan <manish.sajwan@protonmail.com>
2022-11-01 23:45:24 +0530

    add Todo to state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import { useState } from "react";
 
 function App() {
 
+  const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
-  console.log(todo);
+
+  console.log(todos);
+  function addTodo(e) {
+    e.preventDefault();
+    setTodos ([...todos, todo]);
+  }
+
   return (
-   <div>
-    <label for="todo"> 
-      Todo:
-      <input type="text" name="todo" value={todo} onChange={(e) => setTodo(e.target.value)}/>
- 
-    </label>
+    <div>
+      <form>
+        <label for="todo"> 
+          Todo:
+          <input type="text" name="todo" value={todo} onChange={(e) => setTodo(e.target.value)}/>
+        </label>
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
    </div>
   );
 }
